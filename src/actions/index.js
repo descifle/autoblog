886347ebc96f42e694139be966a6bc618533f9d3
Descifle <descifle_itb@yahoo.com>
2021-01-05 17:33:09 -0500

    initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+// REDUX THUNK ALLOWS ASYNC/AWAIT functions possible
+
+import jsonplaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPosts = () => async dispatch => {
+        const response = await jsonplaceholder.get('/posts')
+
+        dispatch({
+            type: 'FETCH_POSTS',
+            payload: response
+        })
+}
