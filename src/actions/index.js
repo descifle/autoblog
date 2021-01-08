db27c0b01e3864bbc9b9d233748ce87201823ea4
Descifle <descifle_itb@yahoo.com>
2021-01-07 22:40:25 -0500

    changed a couple functions to get data

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,20 @@
 // REDUX THUNK ALLOWS ASYNC/AWAIT functions possible
-
+import _ from 'lodash'
 import jsonplaceholder from '../apis/jsonPlaceholder'
+
+export const fetchPostsAndUsers = () => async ( dispatch, getState) => {
+    await dispatch(fetchPosts())
+    
+    // const uniqueIds = _.uniq(_.map(getState().posts, 'userId'))
+    // console.log(uniqueIds)
+    // uniqueIds.forEach(id => dispatch(fetchUser(id)))
+
+    _.chain(getState().posts)
+    .map('userId')
+    .uniq()
+    .forEach(id => dispatch(fetchUser(id)))
+    .value()
+}
 
 export const fetchPosts = () => async dispatch => {
         const response = await jsonplaceholder.get('/posts')
@@ -18,4 +32,16 @@ export const fetchUser = (id) => async dispatch => {
         type: 'FETCH_USER',
         payload: response.data
     })
+    
 }
+
+// export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch)
+
+// const _fetchUser = _.memoize( async (id, dispatch) => {
+//     const response = await jsonplaceholder.get(`/users/${id}`)
+
+//     dispatch({
+//         type: 'FETCH_USER',
+//         payload: response.data
+//     })
+// })
